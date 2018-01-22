261f052bdf4b792c7e1fb0f6ec1ba6e5fcd4f45d
leonnj1990 <1119679388@qq.com>
2018-01-22 11:56:58 +0800

    test

--- src/ResourceCatalog/Template.class.php
+++ src/ResourceCatalog/Template.class.php
@@ -4,4 +4,45 @@ namespace ResourceCatalog\Model;
 class Template
 {
 
+	protected $id;
+	protected $items;
+
+	public function __construct(int $id = 0)
+    {
+        $this->id = !empty($id) ? $id : 0;
+        $this->items = '';
+    }
+
+    public function __destruct()
+    {
+        unset($this->id);
+        unset($this->items);
+    }
+
+    public function setItems(int $id) : void
+    {
+        $this->id = $id;
+    }
+
+    public function getItems() : int
+    {
+        return $this->id;
+    }
+
+    public function setItems(string $items) : void
+    {
+        $this->items = $items;
+    }
+
+    public function getItems() : string
+    {
+        return $this->items;
+    }
+
+    public function add() : bool
+    {
+    	var_dump("hello world");
+        return true;
+    }
+
 }

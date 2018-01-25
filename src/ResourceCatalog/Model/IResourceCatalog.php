d18c8a3ed6a1466c51b3800b78207454620fc3da
leonnj1990 <1119679388@qq.com>
2018-01-25 12:34:02 +0800

    test

--- src/ResourceCatalog/Model/IResourceCatalog.php
+++ src/ResourceCatalog/Model/IResourceCatalog.php
@@ -4,10 +4,10 @@ namespace ResourceCatalog\Model;
 interface IResourceCatalog
 {
     const TYPE = array(
-        'NULL' => array('id' =>0, 'name' => '0', 'shorthand' => '0'),
-        'ZIYUANMULU1' => array('id' =>1, 'name' => '资源目录1', 'shorthand' => 'ZYML1'),
-        'ZIYUANMULU2' => array('id' =>2, 'name' => '资源目录2', 'shorthand' => 'ZYML2'),
-        'ZIYUANMULU3' => array('id' =>3, 'name' => '资源目录3', 'shorthand' => 'ZYML3')
+        'NULL' => array('id' =>0, 'name' => '0', 'identify' => '0', 'enName' => '0'),
+        'ZIYUANMULU1' => array('id' =>1, 'name' => '资源目录1', 'identify' => 'ZYML1', 'enName' => 'zyml1'),
+        'ZIYUANMULU2' => array('id' =>2, 'name' => '资源目录2', 'identify' => 'ZYML2', 'enName' => 'zyml2'),
+        'ZIYUANMULU3' => array('id' =>3, 'name' => '资源目录3', 'identify' => 'ZYML3', 'enName' => 'zyml3')
     );
 
     const USERGROUP_TYPE_RELATIONSHIP = array(

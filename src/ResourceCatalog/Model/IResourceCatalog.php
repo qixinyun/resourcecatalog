e5fcbf32cc651ca91da16c064a5f986555552c7a
leonnj1990 <1119679388@qq.com>
2018-01-28 19:04:27 +0800

    delete mock data

--- src/ResourceCatalog/Model/IResourceCatalog.php
+++ src/ResourceCatalog/Model/IResourceCatalog.php
@@ -5,8 +5,6 @@ interface IResourceCatalog extends IResourceCatalogTypeName, IResourceCatalogTyp
 {
     const TYPE = array(
         'NULL' => 0,
-        'ZYML1' => 1,
-        'ZYML2' => 2,
         'HRBXX' => 3,
         'ZFCGDLJGXX' => 9,
         'QYGSZXXX' => 31,
@@ -15,7 +13,6 @@ interface IResourceCatalog extends IResourceCatalogTypeName, IResourceCatalogTyp
 
     const USERGROUP_TYPE_RELATIONSHIP = array(
         'NULL' => array(self::TYPE['NULL']),
-        '1' => array(self::TYPE['ZYML1'], self::TYPE['ZYML2']),
         '3' => array(self::TYPE['HRBXX']),
         '9' => array(self::TYPE['ZFCGDLJGXX']),
         '31' => array(self::TYPE['QYGSZXXX']),
